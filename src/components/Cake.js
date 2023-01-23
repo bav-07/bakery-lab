211ce933f1580eacf736e7666b6ae8aa431013f9
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-01-23 01:35:13 +0000

    first commit

---
+++ src/components/Cake.js
@@ -0,0 +1,45 @@
+import '../App.css';
+
+const Cake = ({cakeName, ingredients, price, rating, image, className, count, onButtonClick}) => {
+
+    const handleAddToBasket = () => {
+        if (count < 9) {
+            onButtonClick(count + 1);
+        }
+    }
+
+    const handleRemoveFromBasket = () => {
+        if (count > 0) {
+            onButtonClick(count - 1);
+        }
+    }
+
+    let div;
+    if (count === 0) {
+        div = <div className='invisible counter'></div>
+    } else {
+        div = <div className='counter'>{count}</div>
+    }
+
+    return(
+        <>
+            {div}
+            <img className="card_image" src={image} alt={`${cakeName}`}></img>
+            <div className="product_text">
+                <h3>{cakeName}</h3>
+                <p><span className="bold">Ingredients: </span>{ingredients.toString().replaceAll(',',", ")}</p>
+                <p><span className="bold">Price: </span> £{price}.00</p>
+                <p><span className="bold">Rating: </span>{rating} Stars</p>
+            </div>
+            <div className="product_buttons">
+                <button className="add_button" onClick={handleAddToBasket}>Add to Basket</button>
+                <button className="remove_button" onClick={handleRemoveFromBasket}>Remove from Basket</button>
+            </div>
+
+        </>
+    );
+
+
+}
+
+export default Cake;
---
+++ src/containers/CakeContainer.js
@@ -0,0 +1,187 @@
+import { useState } from "react";
+import Cake from "../components/Cake";
+import '../App.css'
+
+const CakeContainer = () => { 
+
+    // console.log(cakes);
+    const states = []
+    // for (let i = 0 ; i < cakes.length ; i++) {
+    //     states.add(useState(0));
+    // }
+
+    // Victora Sponge
+    const cakes = [{
+        cakeName: "Victoria Sponge",
+        ingredients: [
+            "eggs",
+            "butter",
+            "sugar",
+            "self-raising flour",
+            "baking powder",
+            "milk"
+        ],
+        price: 5,
+        rating: 5,
+        image: "https://www.freeimageslive.co.uk/image/view/12347/_original",
+        state: useState(0)
+    },
+
+    // Tea Loaf
+    {
+        cakeName: "Tea Loaf",
+        ingredients: [
+            "eggs",
+            "oil",
+            "dried fruit",
+            "sugar",
+            "self-raising flour",
+            "strong tea",
+        ],
+        price: 2,
+        rating: 3,
+        image: "https://cdn.mos.cms.futurecdn.net/ZNDoeYFevq8rvxYhJJLAjR.jpg",
+        state: useState(0)
+
+    },
+
+    // Carrot Cake
+    {
+        cakeName: "Carrot Cake",
+        ingredients: [
+            "carrots",
+            "walnuts",
+            "oil",
+            "cream cheese",
+            "flour",
+            "sugar",
+        ],
+        price: 8,
+        rating: 5,
+        image: "https://cdn.stocksnap.io/img-thumbs/960w/carrot-cake_A48UPBB6XM.jpg",
+        state: useState(0)
+    },
+    {
+        cakeName: "Victoria Sponge",
+        ingredients: [
+            "eggs",
+            "butter",
+            "sugar",
+            "self-raising flour",
+            "baking powder",
+            "milk"
+        ],
+        price: 5,
+        rating: 5,
+        image: "https://www.freeimageslive.co.uk/image/view/12347/_original",
+        state: useState(0)
+    },
+
+    // Tea Loaf
+    {
+        cakeName: "Tea Loaf",
+        ingredients: [
+            "eggs",
+            "oil",
+            "dried fruit",
+            "sugar",
+            "self-raising flour",
+            "strong tea",
+        ],
+        price: 2,
+        rating: 3,
+        image: "https://cdn.mos.cms.futurecdn.net/ZNDoeYFevq8rvxYhJJLAjR.jpg",
+        state: useState(0)
+
+    },
+
+    // Carrot Cake
+    {
+        cakeName: "Carrot Cake",
+        ingredients: [
+            "carrots",
+            "walnuts",
+            "oil",
+            "cream cheese",
+            "flour",
+            "sugar",
+        ],
+        price: 8,
+        rating: 5,
+        image: "https://cdn.stocksnap.io/img-thumbs/960w/carrot-cake_A48UPBB6XM.jpg",
+        state: useState(0)
+
+    },
+    {
+        cakeName: "Victoria Sponge",
+        ingredients: [
+            "eggs",
+            "butter",
+            "sugar",
+            "self-raising flour",
+            "baking powder",
+            "milk"
+        ],
+        price: 5,
+        rating: 5,
+        image: "https://www.freeimageslive.co.uk/image/view/12347/_original",
+        state: useState(0)
+    },
+
+    // Tea Loaf
+    {
+        cakeName: "Tea Loaf",
+        ingredients: [
+            "eggs",
+            "oil",
+            "dried fruit",
+            "sugar",
+            "self-raising flour",
+            "strong tea",
+        ],
+        price: 2,
+        rating: 3,
+        image: "https://cdn.mos.cms.futurecdn.net/ZNDoeYFevq8rvxYhJJLAjR.jpg",
+        state: useState(0)
+    },
+
+    // Carrot Cake
+    {
+        cakeName: "Carrot Cake",
+        ingredients: [
+            "carrots",
+            "walnuts",
+            "oil",
+            "cream cheese",
+            "flour",
+            "sugar",
+        ],
+        price: 8,
+        rating: 5,
+        image: "https://cdn.stocksnap.io/img-thumbs/960w/carrot-cake_A48UPBB6XM.jpg",
+        state: useState(0)
+    }]  
+
+
+    return(
+        <>
+            <h2>Cakes</h2>
+            <div>
+                <h3>Basket: £{cakes.reduce(function(total, cake) {
+                    return cake.state[0] * cake.price + total; 
+                }, 0)}.00</h3>
+            </div>
+            <div className="flexbox">
+                {cakes.map(function(cake) {
+                    return <div className="card"><Cake cakeName={cake.cakeName} ingredients={cake.ingredients} price={cake.price} rating={cake.rating} image={cake.image} count={cake.state[0]} onButtonClick={cake.state[1]}/></div>
+                })}
+            </div>
+            {/* <Cake cakeName={cakes[0].cakeName} ingredients={cakes[0].ingredients} price={5} rating={5}/>
+            <Cake cakeName={"Tea Loaf"} ingredients={["eggs","oil","dried fruit","sugar", "self-raising flour", "strong tea"]} price={2} rating={3}/>
+            <Cake cakeName={"Victoria Sponge"} ingredients={["carrots","walnuts","oil","cream cheese", "flour", "sugar"]} price={8} rating={5}/> */}
+
+        </>
+    )
+}
+
+export default CakeContainer;
